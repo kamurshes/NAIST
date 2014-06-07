73a940a2919b0eaed33b50d60475b59d8eec64c9
kawamura@piccolospa.org <kawamura@piccolospa.org>
2014-06-07 15:34:55 +0900

    NAIST用GitHub作成

---
+++ 数式_グラフ/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(0, 539)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "1次関数"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(944, 533)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(81, 539)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "三角関数"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(162, 539)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "平方根"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(243, 539)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "グラフの保存"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(944, 562)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+
+End Class
---
+++ 数式_グラフ/Form1.vb
@@ -0,0 +1,147 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Class Form1
+    ''' <summary>
+    ''' 関数（y=f(x)）のデリゲート
+    ''' </summary>
+    ''' <param name="x">入力</param>
+    ''' <returns>出力</returns>
+    ''' <remarks></remarks>
+    Public Delegate Function FuncHandler(ByVal x As Integer) As Integer
+
+    ''' <summary>
+    ''' </summary>
+    ''' <param name="targetImage">描画対象のイメージ</param>
+    ''' <param name="fnc">関数（y=f(x)）</param>
+    ''' <param name="xFrom">Ｘ座標の最小値</param>
+    ''' <param name="xTo">Ｘ座標の最大値</param>
+    ''' <remarks></remarks>
+
+    Public Shared Sub DrawFunction( _
+        ByVal targetImage As Image, ByVal fnc As FuncHandler, _
+        ByVal xFrom As Integer, ByVal xTo As Integer)
+
+        Dim g As Graphics = Nothing
+
+        Try
+            g = Graphics.FromImage(targetImage)
+            g.SmoothingMode = SmoothingMode.AntiAlias
+            '描画領域を初期化（白で塗りつぶす）
+            g.FillRectangle(Brushes.White, 0, 0, _
+                targetImage.Width, targetImage.Height)
+
+            '描画領域の原点を中央に移動
+            Dim xMax As Integer = targetImage.Width * 0.5
+            Dim xMin As Integer = -1 * xMax
+            Dim yMax As Integer = targetImage.Height * 0.5
+            Dim yMin As Integer = -1 * yMax
+            g.TranslateTransform(xMax, yMax)
+            '描画領域のＹ座標の向きを逆にする（Ｙ座標の上方向をプラスにする）
+            g.ScaleTransform(1, -1)
+
+            '格子を描画する（水平線）
+            For y As Single = 0 To yMax Step 10
+                g.DrawLine(Pens.Cyan, xMin, y, xMax, y)
+            Next
+            For y As Single = 0 To yMin Step -10
+                g.DrawLine(Pens.Cyan, xMin, y, xMax, y)
+            Next
+
+            '格子を描画する（垂直線）
+            For x As Single = 0 To xMax Step 10
+                g.DrawLine(Pens.CadetBlue, x, yMin, x, yMax)
+            Next
+            For x As Single = 0 To xMin Step -10
+                g.DrawLine(Pens.CadetBlue, x, yMin, x, yMax)
+            Next
+
+            'Ｘ軸を描画
+            g.DrawLine(Pens.Blue, xMin, 0, xMax, 0)
+            'Ｙ軸を描画
+            g.DrawLine(Pens.Blue, 0, yMin, 0, yMax)
+
+            '関数のグラフを描画
+            For x As Integer = xFrom To xTo
+                Try
+                    g.DrawLine( _
+                        Pens.Black, x - 1, _
+                        fnc.Invoke(x - 1), x, fnc.Invoke(x))
+                Catch ex As ArithmeticException
+                    'ゼロ除算、数値演算のオーバーフロー、
+                    '定義されていない演算エラーは無視する
+                End Try
+            Next
+        Finally
+            If Not (g Is Nothing) Then
+                g.Dispose()
+            End If
+        End Try
+    End Sub
+
+    Public Function 一次関数(ByVal x As Integer) As Integer
+        Return x ^ 2 - x - 1
+    End Function
+
+    Public Function 三角関数(ByVal x As Integer) As Integer
+        Return 50 * Math.Sin(x / 15)
+    End Function
+
+    Public Function 平方根(ByVal x As Integer) As Integer
+        Return Math.Sqrt(x)
+    End Function
+
+    Public Function 三次関数(ByVal x As Integer) As Integer
+        Return (1 / 4) * (x ^ 4) - (3 / 2) * (x ^ 2) - 2 * x + 2
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim img As New Bitmap(Me.PictureBox1.Width, Me.PictureBox1.Height)
+
+        '関数の参照をデリゲート変数に格納する
+        '※関数を変更する場合は、デリゲート変数の中身を変更する。
+        Dim f As New FuncHandler(AddressOf Me.一次関数)
+        '関数のグラフを描画する。
+        DrawFunction(img, f, img.Width * -0.5, img.Width * 0.5)
+
+        Me.PictureBox1.Image = img
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim img As New Bitmap(Me.PictureBox1.Width, Me.PictureBox1.Height)
+
+        '関数の参照をデリゲート変数に格納する
+        '※関数を変更する場合は、デリゲート変数の中身を変更する。
+        Dim f As New FuncHandler(AddressOf Me.三角関数)
+        '関数のグラフを描画する。
+        DrawFunction(img, f, img.Width * -0.5, img.Width * 0.5)
+
+        Me.PictureBox1.Image = img
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim img As New Bitmap(Me.PictureBox1.Width, Me.PictureBox1.Height)
+
+        '関数の参照をデリゲート変数に格納する
+        '※関数を変更する場合は、デリゲート変数の中身を変更する。
+        Dim f As New FuncHandler(AddressOf Me.平方根)
+        '関数のグラフを描画する。
+        DrawFunction(img, f, img.Width * -0.5, img.Width * 0.5)
+
+        Me.PictureBox1.Image = img
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim ファイル名 As String
+        Try
+            SaveFileDialog1.ShowDialog()
+            ファイル名 = IO.Path.GetExtension(SaveFileDialog1.FileName)
+            Dim extension As String = IO.Path.GetExtension(SaveFileDialog1.FileName)
+            PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Png)
+        Catch ex As Exception
+            MsgBox("キャンセルが押されました。")
+        End Try
+        
+    End Sub
+End Class
